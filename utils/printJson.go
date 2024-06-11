fd65c32ddd94f567ebcf37f0702d715028407c46
Jayakrishnan M <jayakrishnan1012@gmail.com>
2024-06-12 02:50:44 +0530

    refactor: Improve printJson function to display JSON node as a hierarchical tree

--- utils/printJson.go
+++ utils/printJson.go
@@ -5,57 +5,82 @@ import (
 	"jsonparser/pkg/parser"
 )
 
-// PrintJson prints the parsed JSON node.
+const (
+	Reset       = "\033[0m"
+	Red         = "\033[31m"
+	Green       = "\033[32m"
+	Yellow      = "\033[33m"
+	Blue        = "\033[34m"
+	Purple      = "\033[35m"
+	Cyan        = "\033[36m"
+	White       = "\033[37m"
+	BrightBlack = "\033[90m"
+	BrightBlue  = "\033[94m"
+	BrightCyan  = "\033[96m"
+	BrightWhite = "\033[97m"
+)
+
+// PrintJson prints the parsed JSON node as a hierarchical tree.
 func PrintJson(node parser.Node) {
 	switch v := node.(type) {
 	case *parser.Object:
-		printObject(v, 1)
+		printObject(v, 0, "")
 	case *parser.Array:
-		printArray(v, 1)
+		printArray(v, 0, "")
 	default:
-		fmt.Println("Parsed JSON as an unknown type")
+		fmt.Println(Red + "Parsed JSON as an unknown type" + Reset)
 	}
 }
 
-func printObject(obj *parser.Object, indent int) {
+func printObject(obj *parser.Object, indent int, prefix string) {
 	for key, value := range obj.Pairs {
-		printIndent(indent)
-		fmt.Printf("%s: ", key)
-		printValue(value, indent+1)
+		printIndent(indent, prefix, true)
+		fmt.Printf("%s%s%s:\n", BrightBlue, key, Reset)
+		printValue(value, indent+1, prefix+"|  ")
 	}
 }
 
-func printArray(arr *parser.Array, indent int) {
+func printArray(arr *parser.Array, indent int, prefix string) {
 	for i, value := range arr.Elements {
-		printIndent(indent)
-		fmt.Printf("[%d]: ", i)
-		printValue(value, indent+1)
+		printIndent(indent, prefix, true)
+		fmt.Printf("%s[%d]%s:\n", BrightCyan, i, Reset)
+		printValue(value, indent+1, prefix+"|  ")
 	}
 }
 
-func printValue(value parser.Value, indent int) {
+func printValue(value parser.Value, indent int, prefix string) {
 	switch v := value.(type) {
 	case *parser.StringLiteral:
-		fmt.Printf("String: %s\n", v.Value)
+		printIndent(indent, prefix, false)
+		fmt.Printf("%sString: %s%s%s\n", Green, Yellow, v.Value, Reset)
 	case *parser.IntegerLiteral:
-		fmt.Printf("Integer: %d\n", v.Value)
+		printIndent(indent, prefix, false)
+		fmt.Printf("%sInteger: %s%d%s\n", Green, Yellow, v.Value, Reset)
 	case *parser.BooleanLiteral:
-		fmt.Printf("Boolean: %t\n", v.Value)
+		printIndent(indent, prefix, false)
+		fmt.Printf("%sBoolean: %s%t%s\n", Green, Yellow, v.Value, Reset)
 	case *parser.NullLiteral:
-		fmt.Println("Null")
+		printIndent(indent, prefix, false)
+		fmt.Printf("%sNull%s\n", Green, Reset)
 	case *parser.Object:
-		fmt.Println("Object:")
-		printObject(v, indent+1)
+		printIndent(indent, prefix, false)
+		fmt.Printf("%sObject:%s\n", Purple, Reset)
+		printObject(v, indent+1, prefix+"|  ")
 	case *parser.Array:
-		fmt.Println("Array:")
-		printArray(v, indent+1)
+		printIndent(indent, prefix, false)
+		fmt.Printf("%sArray:%s\n", Purple, Reset)
+		printArray(v, indent+1, prefix+"|  ")
 	default:
-		fmt.Printf("Unknown type\n")
+		printIndent(indent, prefix, false)
+		fmt.Println(Red + "Unknown type" + Reset)
 	}
 }
 
-func printIndent(indent int) {
-	for i := 0; i < indent; i++ {
-		fmt.Print("  ")
+func printIndent(_ int, prefix string, isKey bool) {
+	fmt.Print(prefix)
+	if isKey {
+		fmt.Print("+--")
+	} else {
+		fmt.Print("|  ")
 	}
 }
